8c79dcef05f931477a754e562e0c3dabfd5b303b
tokyolem <heenssss@mail.ru>
2022-10-03 01:15:23 +0300

    feat: action bar in sign in activity hided

--- app/src/main/java/com/ftclub/footballclub/SignInActivity.kt
+++ app/src/main/java/com/ftclub/footballclub/SignInActivity.kt
@@ -1,20 +1,25 @@
 package com.ftclub.footballclub
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import com.ftclub.footballclub.databinding.ActivitySignInBinding
 
 class SignInActivity: AppCompatActivity() {
 
-    private lateinit var navController: NavController
+    private lateinit var binding: ActivitySignInBinding
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_sign_in)
-        navController = findNavController(R.id.nav_host_fragment)
+
+        findNavController(R.id.nav_host_fragment)
 
         supportActionBar?.hide()
     }
+
+    override fun onBackPressed() {}
 }
