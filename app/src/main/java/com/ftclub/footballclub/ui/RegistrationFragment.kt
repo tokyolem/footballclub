210bb5cc18a2b6e2d342fba3cced05728657a835
tokyolem <heenssss@mail.ru>
2022-10-03 01:22:55 +0300

    refact(RegistarionFragment): code refactored

--- app/src/main/java/com/ftclub/footballclub/ui/RegistrationFragment.kt
+++ app/src/main/java/com/ftclub/footballclub/ui/RegistrationFragment.kt
@@ -21,7 +21,6 @@ class RegistrationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-
         val view = inflater.inflate(R.layout.fragment_registration, container, false)
 
         return view
@@ -29,6 +28,10 @@ class RegistrationFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        navigation()
+    }
+
+    private fun navigation() {
         val toSignInButton = requireActivity().findViewById<FrameLayout>(R.id.to_home_page_from_registration_page)
         toSignInButton.setOnClickListener {
             findNavController().navigate(R.id.action_registrationFragment_to_homeFragment)
