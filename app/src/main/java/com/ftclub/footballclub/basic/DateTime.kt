1c8574fa341747ad1364c5ef33f510d0089be912
tokyolem <heenssss@mail.ru>
2022-10-12 22:13:04 +0300

    feat: object class to get current date/time added

---
+++ app/src/main/java/com/ftclub/footballclub/basic/DateTime.kt
@@ -0,0 +1,43 @@
+package com.ftclub.footballclub.basic
+
+import java.util.Date
+
+object DateTime {
+
+    private val months = mapOf(
+        "Jun" to "Янв.", "Feb" to "Фев.", "Mar" to "Мар.",
+        "Apr" to "Апр.", "May" to "Май.", "Jun" to "Июн.",
+        "Jul" to "Июл.", "Aug" to "Авг.", "Sep" to "Сен.",
+        "Oct" to "Окт.", "Nov" to "Ноя.", "Dec." to "Дек.",
+    )
+
+    private val daysWeek = mapOf(
+        "Mon" to "Пон.", "Tue" to "Вт.", "Wed" to "Ср.",
+        "Thu" to "Чет.", "Fri" to "Пят.", "Sat" to "Суб.", "Sun" to "Вос.",
+    )
+
+    fun getFormatDateTime(): String {
+        var dateTime = getCurrentDateTime()
+
+        dateTime = dateTime.substring(0, dateTime.indexOf("GMT"))
+
+        for (dayWeek in daysWeek)
+            if (dateTime.contains(dayWeek.key))
+                dateTime = dateTime.replace(dayWeek.key, dayWeek.value)
+
+
+        for (month in months)
+            if (dateTime.contains(month.key))
+                dateTime = dateTime.replace(month.key, month.value)
+
+        return dateTime
+    }
+
+
+    private fun getCurrentDateTime(): String{
+        val date = Date()
+
+        return date.toString()
+    }
+
+}
