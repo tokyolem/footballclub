75fa52f7c14d9accab9235632594927d3b9798b8
tokyolem <heenssss@mail.ru>
2022-11-15 22:19:47 +0300

    refact: onBackPressed method overriding removed

--- app/src/main/java/com/ftclub/footballclub/ui/signInActivity/SignInActivity.kt
+++ app/src/main/java/com/ftclub/footballclub/ui/signInActivity/SignInActivity.kt
@@ -3,20 +3,18 @@ package com.ftclub.footballclub.ui.signInActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ftclub.footballclub.R
+import com.ftclub.footballclub.databinding.ActivitySignInBinding
 
 class SignInActivity: AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignInBinding
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
         supportActionBar?.hide()
     }
-
-    override fun onStart() {
-        super.onStart()
-        window.statusBarColor = resources.getColor(R.color.white, resources.newTheme())
-    }
-
-    override fun onBackPressed() {}
 }
